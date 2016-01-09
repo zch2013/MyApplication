71e084a52dc37af250e670ac3ae351459cf5c6a1
huan <huan517338@163.com>
2016-01-09 17:44:28 +0800

    第二次

--- app/src/main/java/androidsvn/myapplication/MainActivity.java
+++ app/src/main/java/androidsvn/myapplication/MainActivity.java
@@ -3,11 +3,15 @@ package androidsvn.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Text text;
     }
 }
